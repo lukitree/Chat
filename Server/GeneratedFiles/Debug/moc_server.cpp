f697cd98f7301fd2f246222cfcb18ce664bf613a
lukitree <lukitree@gmail.com>
2015-08-06 04:26:03 -0600

    Untracking more unneeded files

--- Server/GeneratedFiles/Debug/moc_server.cpp
+++ Server/GeneratedFiles/Debug/moc_server.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Server_t {
-    QByteArrayData data[10];
-    char stringdata0[95];
+    QByteArrayData data[12];
+    char stringdata0[122];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,20 +30,23 @@ struct qt_meta_stringdata_Server_t {
 static const qt_meta_stringdata_Server_t qt_meta_stringdata_Server = {
     {
 QT_MOC_LITERAL(0, 0, 6), // "Server"
-QT_MOC_LITERAL(1, 7, 11), // "sendMessage"
-QT_MOC_LITERAL(2, 19, 0), // ""
-QT_MOC_LITERAL(3, 20, 7), // "message"
-QT_MOC_LITERAL(4, 28, 11), // "QTcpSocket&"
-QT_MOC_LITERAL(5, 40, 6), // "socket"
-QT_MOC_LITERAL(6, 47, 9), // "sendToAll"
-QT_MOC_LITERAL(7, 57, 10), // "getMessage"
-QT_MOC_LITERAL(8, 68, 13), // "newConnection"
-QT_MOC_LITERAL(9, 82, 12) // "onDisconnect"
+QT_MOC_LITERAL(1, 7, 13), // "SEND_UserList"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 11), // "sendMessage"
+QT_MOC_LITERAL(4, 34, 7), // "message"
+QT_MOC_LITERAL(5, 42, 11), // "QTcpSocket&"
+QT_MOC_LITERAL(6, 54, 6), // "socket"
+QT_MOC_LITERAL(7, 61, 9), // "sendToAll"
+QT_MOC_LITERAL(8, 71, 10), // "getMessage"
+QT_MOC_LITERAL(9, 82, 13), // "newConnection"
+QT_MOC_LITERAL(10, 96, 12), // "onDisconnect"
+QT_MOC_LITERAL(11, 109, 12) // "sendUserList"
 
     },
-    "Server\0sendMessage\0\0message\0QTcpSocket&\0"
-    "socket\0sendToAll\0getMessage\0newConnection\0"
-    "onDisconnect"
+    "Server\0SEND_UserList\0\0sendMessage\0"
+    "message\0QTcpSocket&\0socket\0sendToAll\0"
+    "getMessage\0newConnection\0onDisconnect\0"
+    "sendUserList"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,23 +56,31 @@ static const uint qt_meta_data_Server[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    2,   39,    2, 0x08 /* Private */,
-       6,    1,   44,    2, 0x08 /* Private */,
-       7,    0,   47,    2, 0x08 /* Private */,
-       8,    0,   48,    2, 0x08 /* Private */,
-       9,    0,   49,    2, 0x08 /* Private */,
+       3,    2,   50,    2, 0x08 /* Private */,
+       7,    1,   55,    2, 0x08 /* Private */,
+       8,    0,   58,    2, 0x08 /* Private */,
+       9,    0,   59,    2, 0x08 /* Private */,
+      10,    0,   60,    2, 0x08 /* Private */,
+      11,    0,   61,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::QString, 0x80000000 | 4,    3,    5,
-    QMetaType::Void, QMetaType::QString,    3,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 5,    4,    6,
+    QMetaType::Void, QMetaType::QString,    4,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,12 +94,23 @@ void Server::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         Server *_t = static_cast<Server *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->sendMessage((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QTcpSocket(*)>(_a[2]))); break;
-        case 1: _t->sendToAll((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 2: _t->getMessage(); break;
-        case 3: _t->newConnection(); break;
-        case 4: _t->onDisconnect(); break;
+        case 0: _t->SEND_UserList(); break;
+        case 1: _t->sendMessage((*reinterpret_cast< QString(*)>(_a[1])),(*reinterpret_cast< QTcpSocket(*)>(_a[2]))); break;
+        case 2: _t->sendToAll((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 3: _t->getMessage(); break;
+        case 4: _t->newConnection(); break;
+        case 5: _t->onDisconnect(); break;
+        case 6: _t->sendUserList(); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (Server::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&Server::SEND_UserList)) {
+                *result = 0;
+            }
         }
     }
 }
@@ -118,14 +140,20 @@ int Server::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 7;
     }
     return _id;
+}
+
+// SIGNAL 0
+void Server::SEND_UserList()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
 }
 QT_END_MOC_NAMESPACE
